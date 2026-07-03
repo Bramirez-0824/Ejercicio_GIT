6fac31d4623fd5ba3e3b053d854a36371018dbc4
Bramirez-0824 <bramirez@wrh-honduras.org>
2026-07-03 14:06:00 -0600

    Agregar bucle al menu

--- Ejercicio_GIT/Program.cs
+++ Ejercicio_GIT/Program.cs
@@ -10,28 +10,36 @@ namespace Ejercicio_GIT
     {
         static void Main(string[] args)
         {
-
-            double resultado, num1 = 5,num2 = 10;
-            Console.WriteLine("1. Suma\n" +
-                "2. Resta\n" +
-                "3. Multiplicacion\n" +
-                "Selecciona una opción:");
-            int opcion = Convert.ToInt32(Console.ReadLine());
-            switch (opcion)
+            int opcion;
+            do
             {
-                case 1:
-                    resultado = Suma(num1,num2);
-                    Console.WriteLine(resultado);
-                    break;
-                case 2:
-                    resultado = Resta(num1, num2);
-                    Console.WriteLine(resultado);
-                    break;
-                case 3:
-                    resultado = Multiplicacion(num1, num2);
-                    Console.WriteLine(resultado);
-                    break;
-            }
+                double resultado, num1 = 5, num2 = 10;
+                Console.WriteLine("1. Suma\n" +
+                    "2. Resta\n" +
+                    "3. Multiplicacion\n" +
+                    "4. Salir\n" +
+                    "Selecciona una opción:");
+                opcion = Convert.ToInt32(Console.ReadLine());
+                switch (opcion)
+                {
+                    case 1:
+                        resultado = Suma(num1, num2);
+                        Console.WriteLine(resultado);
+                        break;
+                    case 2:
+                        resultado = Resta(num1, num2);
+                        Console.WriteLine(resultado);
+                        break;
+                    case 3:
+                        resultado = Multiplicacion(num1, num2);
+                        Console.WriteLine(resultado);
+                        break;
+                    case 4:
+                        Console.WriteLine("Saliendo del programa");
+                        break;
+                }
+            } while (opcion != 4);
+
         }
 
         static double Suma(double num1,double num2)
